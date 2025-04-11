26cc5d379974b25dfe1f63e444f5f93edc051271
alwadu <you@example.com>
2025-04-11 22:35:47 +0600

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import Header from './component/header'
+
+function App() {
+  
+
+  return (
+    <>
+      
+     <Header></Header>
+      
+    </>
+  )
+}
+
+export default App
